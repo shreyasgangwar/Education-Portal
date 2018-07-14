39ee682ab771e3d787e2135b5d95cf31cf29f1cd
Satyam Sharma <satyam04sharma@gmail.com>
2018-07-14 19:59:14 +0530

    Skill development program

---
+++ skilldevelopmentprog.php
@@ -0,0 +1,313 @@
+<html>
+    <head>
+        <title>Educot-Skill Development Program</title>
+ 
+        <script type="text/javascript" src="jquery.min.js"></script>
+        
+        <link rel="stylesheet" href="style/slider-def.css">
+		<link href="css/main.css" rel="stylesheet" type="text/css">
+		
+     <link href="https://fonts.googleapis.com/css?family=Mirza" rel="stylesheet">  
+        
+        <!-- Bootstrap CSS -->
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+        <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <script src="https://use.fontawesome.com/9adc8cef54.js"></script>
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"> 
+	  <style>
+		body{
+			
+			font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+		}
+		.navbar {
+            
+            
+            background: rgba(0, 0, 0, 0.78); //NON-IE
+			
+        }
+		footer{
+			background: #ffffff;
+		}
+  </style>
+    </head>
+<body>
+    
+    
+     <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse">
+ 
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    
+              <span class="navbar-toggler-icon"></span>
+  
+          </button>
+      
+      <div class="container">
+      <div class="row">
+          <a class="navbar-brand" href="index.php"><img class="img-responsive" src="images/logo.png" alt="logo"></a>
+      <h2 style="color:white;" class="educot-logo">educot</h2>
+      </div></div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+              <ul class="navbar-nav ml-auto">
+      
+                  <li class="nav-item active">
+        
+                      <a class="nav-link" href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home<span class="sr-only">(current)</span></a>
+      
+                  </li>
+      
+                 <li class="nav-itemw active dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fa fa-book" aria-hidden="true"></i>Programs offered
+                    </a>
+                    <div class="dropdown-menu row" aria-labelledby="navbarDropdown">
+            <div class="container">
+            <div class="row">
+              <div class="col-md-3">
+                <span class="text-uppercase">Programs</span>
+                <div class="dropdown-divider"></div>
+                <ul class="nav flex-column">
+                <li class="dropdown-item">
+                  <a class="" href="#">Educot's placement program</a>
+                </li>
+                <li class="dropdown-item">
+                  <a class="" href="#">Educot's skill development program</a>
+                </li>
+                <li class="dropdown-item">
+                  <a class="" href="#">Educot's School student internship program</a>
+                </li>
+                
+                
+                </ul>
+              </div>
+              
+            </div>
+            </div>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item disabled" href="#">More Programs coming soon</a>
+                    </div>
+                  </li>
+      
+                  <li class="nav-item active">
+       
+                      <a class="nav-link" href="hire.php"><i class="fa fa-superpowers" aria-hidden="true"></i> Hire Talent</a>
+      
+                  </li>
+                  
+
+                  <li class="nav-item active">
+       
+                      <a class="nav-link divider" href="instructorRegister.php"><i class="fa fa-envelope" aria-hidden="true"></i> Instructors</a>
+            
+                  </li>              
+          
+          <a href="login.php" class="btn btn-primary" type="button">Login/SignUp</a>
+          
+
+                  
+                  
+
+   
+              </ul>
+  
+          </div>
+
+      </nav>
+       <div class="programintro p-overview-background">
+	  
+	  <div class="p-overview-tint">
+  <div class="container">
+     	
+    <h1>Don't let any stone unturned </h1>
+    <h5>Prepare for your competitive exam and analyse your competition and your preparation with company name exam preparation program.</h5>
+  	
+  </div>
+  </div>
+  
+</div>
+<section class="abt2">
+	<h1 class="feature_heading2 educot-logo" >Educot’s skill development program</h1>
+	<h2 class="feature_heading2 educot-logo" >Project Based Training with EDUCOT<br> ( WORK FORM HOME )
+</h2>
+	<div style="margin:20px;">
+        <h3 class="feature_heading display-6">Project based training with educot is a program is a 1 month program in which we will give projects to you to enhance your practical approach towards the problem. In this program we will try to develop skills in you through project based learning so that you don't only have bookish knowledge but know how to apply that knowledge.</h3>
+        <h5 class="feature_faq mb-4 " style="color:#424242">Areas where we provide projects : <br>
+          •  Computer science areas.<br>
+          •  Marketing and management.<br>
+          •  Content writing.<br>
+•  Video editing.<br>
+•  Research and development.<br>
+
+        </h5>
+        <h3 class="feature_heading display-6">Work on the ongoing projects of different companies from home so that you will get the knowledge on how things are done in the corporate world after graduating from college.<br>
+In this program you can register as a team or as an individual.
+After the completion of task we will award you a certificate and can get more opportunities to work on the different projects<br>
+After graduating you will have enough professional skills and experience to get job in first interview itself. Also if you do surprisingly well, you can also get Pre-Placement Offer by different companies.<br>
+</h3>
+<h5 class="feature_faq mb-4 " style="color:#424242">Benefits : <br>
+          •  Get to work on various projects.<br>
+          •  Improved CV.<br>
+          •  Work experience.<br>
+•  Employbility Skills .<br>
+•  Certificate .<br>
+•  Pre-Placement Offer(PPO).<br>
+•  Guidance and support from the mentor .<br>
+•  Be a project leader in mega projects in which team is participated..<br>
+
+        </h5>
+        <h5 class="feature_faq mb-4 " style="color:#424242">PROCESS OF SELECTION : <br>
+          1)	Give a test and enroll with EDUCOT.<br>
+2)	Select a project feasible for you. <br>
+3)	Register for respective project. <br>
+4)	Start working on project.<br>
+
+
+        </h5>
+        <h5 class="feature_faq mb-4 " style="color:#424242">LIFE CYCLE OF PROGRAM  : <br>
+          1.	Project topics are given to the student with a finish deadline<br>
+2.	Mentor will keep reviewing it after every week. With the help of report.<br>
+3.	After completion of the entire project you have to submit the report and the link of presentation ( recorded with screen recorder).<br>
+4.	After this the certificate and ratings will be issued by educot. Ratings will be mentioned in certificate.<br>
+
+</h5>
+        <h4 class="feature_heading display-6">
+        WhatsApp Contact no.: 8979538455<br></h4>
+        </div>
+  <div style="margin:20px;">
+        
+        <h5 class="feature_heading display-6" style="color:#424242">Click the given below link to register for the program</h5>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeF7gjBfR1Nsy3n-g8DoUKNPUWkGit8plSFR8iZJuJRDweJEw/viewform"> https://docs.google.com/forms/d/e/1FAIpQLSeF7gjBfR1Nsy3n-g8DoUKNPUWkGit8plSFR8iZJuJRDweJEw/viewform</a>
+        
+        </div>
+
+</section>
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">© 2017 Educot. All Rights Reserved </span>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center center-block">
+       
+					
+							<a href="https://www.facebook.com/Jump2school/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+							<a href="https://www.youtube.com/channel/UCZW65ojZVSWB5Ct9UYZymcg"><i id="social-tw" class="fa fa-youtube-square fa-3x social"></i></a>
+							
+						</div>
+                </div>
+				
+                <div class="col-md-4">
+					<span>| Design by <a href="https://www.facebook.com/aman.r.singh.50">Advanced Tribe</a></span>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+	    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+      
+      <script type="text/javascript">
+    function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+        if($(window).scrollTop() > startY){
+            $('.navbar').addClass("scrolled");
+        }else{
+            $('.navbar').removeClass("scrolled");
+        }
+    }
+            
+    
+
+    if($('.navbar').length > 0){
+        $(window).on("scroll load resize", function(){
+            checkScroll();
+        
+        });
+    }
+	
+	$('.navbar-toggler').click(function(){
+		var startY = $('.navbar').height() * 2;
+		if($(window).scrollTop() < startY){
+            $('.navbar').toggleClass("scrolled");
+        }		
+	});
+            
+		// typer for hello
+		window.onload = function() {
+			var elements = document.getElementsByClassName('txt-rotate');
+			for (var i = 0; i < elements.length; i++) {
+				var toRotate = elements[i].getAttribute('data-rotate');
+				var period = elements[i].getAttribute('data-period');
+				if (toRotate) {
+					new TxtRotate(elements[i], JSON.parse(toRotate), period);
+				}
+			}
+			// INJECT CSS
+			var css = document.createElement("style");
+			css.type = "text/css";
+			css.innerHTML = ".txt-rotate > .wrap { border-right: 10px solid #ffffff }";
+			document.body.appendChild(css);
+		};
+
+		var TxtRotate = function(el, toRotate, period) {
+			this.toRotate = toRotate;
+			this.el = el;
+			this.loopNum = 0;
+			this.period = parseInt(period, 1) || 1000;
+			this.txt = '';
+			this.tick();
+			this.isDeleting = false;
+		};
+
+		TxtRotate.prototype.tick = function() {
+			var i = this.loopNum % this.toRotate.length;
+			var fullTxt = this.toRotate[i];
+
+			if (this.isDeleting) {
+				this.txt = fullTxt.substring(0, this.txt.length - 1);
+			} else {
+				this.txt = fullTxt.substring(0, this.txt.length + 1);
+			}
+
+			this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+			var that = this;
+			var delta = 200 - Math.random() * 100;
+
+			if (this.isDeleting) {
+				delta /= 2;
+			}
+
+			if (!this.isDeleting && this.txt === fullTxt) {
+				delta = this.period;
+				this.isDeleting = true;
+			} else if (this.isDeleting && this.txt === '') {
+				this.isDeleting = false;
+				this.loopNum++;
+				delta = 200;
+			}
+
+			setTimeout(function() {
+				that.tick();
+			}, delta);
+		};
+
+
+    </script>
+    </body>
+</html>	
