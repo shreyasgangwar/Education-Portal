d577b755b18629503da9c778389b1ff1b0264214
Nikhil_kumar <nikhil14091997@gmail.com>
2018-09-07 11:50:28 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+echo "YOU are not logged in as administrator of page";
+echo "please login with valid amin email and password";
+  ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title>redirecting page</title>
+  </head>
+  <body>
+  <a href="uploadmarks.php">redirect tab</a>
+  </body>
+  </html>
